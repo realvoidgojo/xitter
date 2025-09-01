8b407f272e3c6c4debd12dda54bb163df3a305dd
realvoidgojo <dir.harishsivaraman@gmail.com>
2025-09-01 21:10:35 +0530

    Springboot Boilerplate Initialized

---
+++ xitter-backend/src/main/java/com/xitter/XitterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XitterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XitterBackendApplication.class, args);
+	}
+
+}
---
+++ xitter-backend/src/test/java/com/xitter/XitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
